6fd5083f7e8aeffe0048f45dfedc924bf960c3a2
David Mannes <git@davidmannes.de>
2022-11-06 20:23:04 +0100

    Add market building (no functionality yet)

---
+++ Assets/Market.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+using Tycoon.Buildings;
+
+namespace Tycoon.Assets;
+
+public class Market : IBlueprint
+{
+	public string Name => "Market";
+	public int Cost => 0;
+	public Texture2D Texture { get; } = GD.Load<Texture2D>("res://Assets/market.png");
+
+	public Shape2D Shape { get; } = new RectangleShape2D
+	{
+		Size = new Vector2(1024, 1024),
+	};
+}
