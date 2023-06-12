b4ea37451e6bca969f8f38aed2e87667e55cb122
David Mannes <git@davidmannes.de>
2023-06-12 10:28:01 +0200

    Allow deleting entities

---
+++ Systems/NodeRemovalSystem.cs
@@ -0,0 +1,19 @@
+﻿using DefaultEcs;
+using DefaultEcs.System;
+using Godot;
+
+namespace Tycoon.Systems;
+
+public class NodeRemovalSystem : ISystem<double>
+{
+	public NodeRemovalSystem(World world)
+	{
+		world.SubscribeComponentRemoved((in Entity _, in Node2D node) => node.QueueFree());
+	}
+
+	public void Dispose() { }
+
+	public void Update(double state) { }
+
+	public bool IsEnabled { get; set; }
+}
